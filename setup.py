fbcdab072a88144d67b01c44f30c90ca229ffe2d
Kristobal Junta <junta.kristobal@gmail.com>
2019-09-23 15:47:55 +0300

    add setuptools config

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scrapy_new",
+    version="0.0.1",
+    author="Kristobal Junta",
+    author_email="junta.kristobal@gmail.com",
+    description="A package providing code generation command for scrapy CLI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KristobalJunta/scrapy-command-new",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
